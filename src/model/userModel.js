6ab75d75f54b48d2b11c28b5d93f9dc3f93af113
ritik <tangriritik30@gmail.com>
2022-09-14 17:35:44 +0530

    changes

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 const ObjectId= mongoose.Schema.Types.ObjectId
 
 
-const userScehma = new mongoose.Schema({
-title : {type:String, required:true, enum: ["Mr", "Mrs", "Miss"],},
-fullName :  {type:String, required:true,},
-email : {type:String,required:true,unique:true},
-password: {type:String,required:true,},
-date: {type: Date,default: Date.now},
+// const userScehma = new mongoose.Schema({
+// title : {type:String, required:true, enum: ["Mr", "Mrs", "Miss"],},
+// fullName :  {type:String, required:true,},
+// email : {type:String,required:true,unique:true},
+// password: {type:String,required:true,},
+// date: {type: Date,default: Date.now},
 
 
-}, { timestamps: true });
+// }, { timestamps: true });
 
 
 module.exports = mongoose.model('userModel', userScehma)
